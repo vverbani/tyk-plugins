92b60ffb1838b9481c291232504f98594a1e2f9b
Peter Stubbs <peter.stubbs@outlook.com>
2020-07-04 20:22:24 +0100

    signed jwt!

--- jwt/tyk-jwt.go
+++ jwt/tyk-jwt.go
@@ -1,49 +1,119 @@
 package main
 
 import (
-	//"gopkg.in/square/go-jose.v2/jwt"
-
+	"crypto/rsa"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/TykTechnologies/tyk/log"
-	"gopkg.in/square/go-jose.v2/json"
-	//"encoding/json"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 type jwtConfig struct {
-	jwtCertFile   string   `json:"jwtCertFile"`
-	jwtKeyFile    string   `json:"jwtKeyFile"`
-	jwksCertFiles []string `json:"jwksCertFiles"`
+	JwtCertFile   string   `json:"jwtCertFile"`
+	JwtKeyFile    string   `json:"jwtKeyFile"`
+	JwksCertFiles []string `json:"jwksCertFiles"`
 }
 
-var logger = log.Get()
+var (
+	jwtPublicCert *rsa.PublicKey
+	jwtPrivateKey *rsa.PrivateKey
+	logger        = log.Get()
+)
 
 func init() {
 	configFileName := "/opt/tyk-plugins/tyk-jwt.json"
+
+	// read the config file
 	var config jwtConfig
 	logger.Info("jtw-plugin: Loading config file: ", configFileName)
 	configFile, err := os.Open(configFileName)
-	if err != nil {
-		logger.Error("jtw-plugin: Cannot load ", configFileName, " : ", err)
-		os.Exit(1)
-	}
+	logErr("Error", "jtw-plugin: Cannot load "+configFileName+": ", err)
 	defer configFile.Close()
+	// Parse the config file
 	jsonParser := json.NewDecoder(configFile)
 	err = jsonParser.Decode(&config)
-	if err != nil {
-		logger.Warning("Parsing ", configFile, " gave error: ", err)
-	} else {
-		logger.Info("jwtCertFile is: ", config.jwtCertFile)
-		logger.Info("jwtKeyFile is: ", config.jwtKeyFile)
-		logger.Info("jwksCertFiles are: ", config.jwksCertFiles)
+	logErr("Error", "Parsing "+configFileName+" gave error: ", err)
+
+	// report on the values loaded
+	logger.Info("jwtCertFile is: ", config.JwtCertFile)
+	logger.Info("jwtKeyFile is: ", config.JwtKeyFile)
+	for i := range config.JwksCertFiles {
+		logger.Info("jwksCertFiles are: ", config.JwksCertFiles[i])
 	}
+
+	// start of jwt stuff
+	//token := jwt.New(jwt.SigningMethodRS256)
+	jwtKeyBytes, err := ioutil.ReadFile(config.JwtKeyFile)
+	logErr("Error", "Opening JwtKeyFile "+config.JwtKeyFile+" failed: ", err)
+	jwtPrivateKey, err = jwt.ParseRSAPrivateKeyFromPEM(jwtKeyBytes)
+	logErr("Error", "ParseRSAPrivateKeyFromPEM failed with error: ", err)
+	logger.Info("jwtPrivateKey is: ", jwtPrivateKey) // lets not log our private key
+
+	jwtCertBytes, err := ioutil.ReadFile(config.JwtCertFile)
+	logErr("Error", "Opening JwtKeyFile "+config.JwtCertFile+" failed: ", err)
+	jwtPublicCert, err = jwt.ParseRSAPublicKeyFromPEM(jwtCertBytes)
+	logErr("Error", "ParseRSAPublicKeyFromPEM failed with error: ", err)
+	logger.Info("jwtPublicCert is: ", jwtPublicCert)
+}
+
+func logErr(level, message string, err error) {
+	if err != nil {
+		switch level {
+		case "Trace":
+			logger.Trace(message, err)
+		case "Debug":
+			logger.Debug(message, err)
+		case "Info":
+			logger.Info(message, err)
+		case "Warning":
+			logger.Warning(message, err)
+		case "Error":
+			logger.Error(message, err)
+		case "Fatal":
+			logger.Fatal(message, err)
+			os.Exit(1)
+		case "Panic":
+			logger.Panic(message, err)
+			os.Exit(1)
+		default:
+			logger.Info("LogErr: Unknown Log level "+level+"( "+message+") ", err)
+		}
+	}
+}
+
+type myClaims struct {
+	Nbf int64 `json:"nbf"`
+	jwt.StandardClaims
 }
 
 // AddFooBarHeader adds custom "Foo: Bar" header to the request
 func AddFooBarHeader(rw http.ResponseWriter, r *http.Request) {
 	logger.Info("Processing HTTP request in Golang plugin!!")
-	r.Header.Add("Foo", "Bar")
+	claims := myClaims{
+		Nbf: time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	ss, err := token.SignedString(jwtPrivateKey)
+	logErr("Info", "token.SignedString(jwtPrivateKey) ", err)
+	/*
+		t := jwt.New(jwt.GetSigningMethod("RS256"))
+		t.Claims["AccessToken"] = "level1"
+		t.Claims["CustomUserInfo"] = struct {
+			Name string
+			Kind string
+		}{user, "human"}
+		t.Claims["exp"] = time.Now().Add(time.Minute * 1).Unix()
+		tokenString, err := t.SignedString(signKey) */
+
+	r.Header.Add("JWT", ss)
 }
 
 func main() {}
+
+// docker run --rm -v /C//Users/pstubbs/go/src/mine/tyk-plugins/jwt:/plugin-source tykio/tyk-plugin-compiler:v2.9.4.2 tyk-jwt.so
+// cp .\tyk-jwt.so C:\Users\pstubbs\tyk\plugins\2.9.4.2\
+// docker container restart sandbox-1
