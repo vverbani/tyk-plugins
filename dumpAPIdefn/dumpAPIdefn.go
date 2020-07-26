f3e5f3017112f3474fa197b635b77bff28511d28
Peter Stubbs <peter.stubbs@outlook.com>
2020-07-26 12:45:26 +0100

    api config dump plugin

---
+++ dumpAPIdefn/dumpAPIdefn.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+  "github.com/TykTechnologies/tyk/ctx"
+  "github.com/davecgh/go-spew/spew"
+)
+
+// DumpAPIdefn dumps out the API def visible to the pulgin so we can see what we've got
+func DumpAPIdefn(rw http.ResponseWriter, r *http.Request) {
+
+  apidef := ctx.GetDefinition(r)
+  log.Println("Start API definition dump")
+  spew.Dump(apidef)
+  log.Println("End API definition dump")
+
+}
+
+func main() {}
