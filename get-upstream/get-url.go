af0d955c1fdf49c0337b9c80258634d8a24ced68
Peter Stubbs <peter.stubbs@outlook.com>
2020-07-26 09:35:59 +0100

    working

--- get-upstream/get-url.go
+++ get-upstream/get-url.go
@@ -7,7 +7,7 @@ import (
 
 // ReportURL logs the upstream URL after it has been rewritten
 func ReportURL(rw http.ResponseWriter, r *http.Request) {
-	log.Info("Upstream URL is: ", r.URL)
+	log.Println("Upstream URL is: ", r.URL)
 	//r.Header.Add("Foo", "Bar-mitsva")
 }
 
