f47a5e81e0dc33d93426a32f106217960fb024a6
Peter Stubbs <peter.stubbs@outlook.com>
2021-06-23 19:05:57 +0100

    added a plugin to decode a JWT and use a claim

---
+++ addHeaderFromClaim/addHeaderFromClaim.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/TykTechnologies/tyk/headers"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func stripBearer(token string) string {
+	if len(token) > 6 && strings.ToUpper(token[0:7]) == "BEARER " {
+		return token[7:]
+	}
+	return token
+}
+
+var policyArray = []string{"60d30bc4077b5f00175ad141", "60d30bc4077b5f00175ad142", "60d30bc4077b5f00175ad143", "60d30bc4077b5f00175ad144"}
+var headerVal = []string{"Val1", "Val2", "Val3", "Val4"}
+var headerField = "X-Tyk-Pol-Val"
+
+// addHeaderFromClaim based on Policy ID
+func addHeaderFromClaim(rw http.ResponseWriter, r *http.Request) {
+
+	// 1. Retrieve the 'pol' claim from the JWT used to authenticate
+	// 2. Look that up in policyArray
+  // 3. Find the corresponding headerVal into the the header headerField
+
+	log.Println("Start addHeaderFromClaim Plugin")
+
+	// decode the JWT from the Authorization header
+	jwtStr := stripBearer(r.Header.Get(headers.Authorization))
+	token, _ := jwt.Parse(jwtStr, nil)
+	claims, _ := token.Claims.(jwt.MapClaims)
+
+	// check if the pol claim is populated and is in the array policyArray
+	if val, ok := claims["pol"]; ok {
+		polStr := fmt.Sprintf("%v", val)
+		log.Println("Pol is " + polStr)
+		for k, v := range policyArray {
+			if v == polStr {
+				// add the headerField header corresponding to the policy
+				r.Header.Add(headerField, headerVal[k])
+				break
+			}
+		}
+	}
+
+	log.Println("End addHeaderFromClaim Plugin")
+
+}
+
+func main() {}
