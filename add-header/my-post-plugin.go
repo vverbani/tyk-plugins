13aa9c31b9abfb0e02124ee4add259ca4d3a21de
OldStubbsy <peter@tyk.io>
2021-08-05 13:46:18 +0100

    added warning about causing the gateway to exit

--- add-header/my-post-plugin.go
+++ add-header/my-post-plugin.go
@@ -12,24 +12,25 @@ import (
 func AddHeader(rw http.ResponseWriter, r *http.Request) {
   file, err := os.Open("/opt/tyk-plugins/my-post-plugin.txt")
   if err != nil {
+    // warning. This causes the gateway to exit
     log.Fatalf("failed opening file: %s", err)
   }
   defer file.Close()
- 
+
   scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	var txtlines []string
- 
+
   for scanner.Scan() {
 		txtlines = append(txtlines, scanner.Text())
   }
-  
+
   for _, eachline := range txtlines {
     text := strings.Split(eachline, ": ")
     r.Header.Add(text[0], text[1])
 		//fmt.Println(eachline)
   }
-  
+
   //r.Header.Add("Foo", "Bar-mitsva")
 }
 
